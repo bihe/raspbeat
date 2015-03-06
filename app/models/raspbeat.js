80284c16fa24b15252795ce1619fdc07c57d021c
Henrik Binggl <henrik@binggl.net>
2015-03-06 18:37:45 +0100

    Changed the mongo model :blue_book:

--- app/models/raspbeat.js
+++ app/models/raspbeat.js
@@ -7,64 +7,42 @@ var mongoose = require('mongoose');
 var randomstring = require('randomstring');
 var Schema = mongoose.Schema;
 
-var states = 'dirty done'.split(' ');
-
-var documentSchema = new Schema({
+var raspSchema = new Schema({
   title: {
     type: String,
     trim: true,
     required: 'Title cannot be blank'
   },
-  fileName: {
+  alternativeId: {
+    type: String,
+    trim: true
+  },
+  ip: {
     type: String,
     trim: true,
-    required: 'Filename cannot be blank'
-  },
-  alternativeId: {
-		type: String,
-		trim: true,
-		default: ''
-  },
-  previewLink: {
-		type: String,
-		trim: true,
-		default: ''
-  },
-  amount: {
-    type: Number,
-    default: 0
+    required: 'IP address cannot be blank'
   },
   created: {
 		type: Date,
 		default: Date.now
 	},
-  modified: {
+  localTimestamp: {
     type: Date,
     default: Date.now
-  },
-	senders: [{ type: Schema.Types.ObjectId, ref: 'Sender' }],
-	tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
-  // state of the document
-  state: {
-    type: String,
-    enum: states,
-    default: 'dirty'
   }
-
 });
 
 // add some Middleware magic
-documentSchema.pre('save', function (next) {
+raspSchema.pre('save', function (next) {
   if(!this.alternativeId || this.alternativeId === '') {
 		// create and set an alternative id
 		this.alternativeId = randomstring.generate(8);
   }
-  this.modified = new Date();
   next();
 });
 
-documentSchema.methods.toString = function() {
-	return '[name: ' + this.title + ' (id: ' + this._id +')]';
+raspSchema.methods.toString = function() {
+	return '[title: ' + this.title + ', IP: ' + this.ip + ' (id: ' + this._id +')]';
 };
 
-module.exports = mongoose.model('Document', documentSchema);
+module.exports = mongoose.model('Raspbeat', raspSchema);
